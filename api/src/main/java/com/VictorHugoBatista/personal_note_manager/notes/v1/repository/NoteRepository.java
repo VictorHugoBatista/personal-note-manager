2275e972517d338a7d2dd1015f453f36d40c092b
Victor Hugo Batista <vct.bta@gmail.com>
2025-09-28 17:33:44 -0300

    Apply business rule on the list and detail endpoints
    List doesn't return content and detail doesn't return preview

--- api/src/main/java/com/VictorHugoBatista/personal_note_manager/notes/v1/repository/NoteRepository.java
+++ api/src/main/java/com/VictorHugoBatista/personal_note_manager/notes/v1/repository/NoteRepository.java
@@ -1,8 +1,19 @@
 package com.VictorHugoBatista.personal_note_manager.notes.v1.repository;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import com.VictorHugoBatista.personal_note_manager.notes.v1.model.Note;
 
+@Repository
 public interface NoteRepository extends MongoRepository<Note, String> {
+    @Query(value = "{}", fields="{'content': 0}")
+    public List<Note> findAll();
+
+    @Query(fields="{'preview': 0}")
+    public Optional<Note> findById(String id);
 }
