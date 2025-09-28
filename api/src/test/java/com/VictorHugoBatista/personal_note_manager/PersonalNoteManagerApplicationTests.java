5dfa52a58c908bf34cd18a7c58329cede924ae64
Victor Hugo Batista <vct.bta@gmail.com>
2025-09-27 21:32:27 -0300

    Add initial spring boot code

---
+++ api/src/test/java/com/VictorHugoBatista/personal_note_manager/PersonalNoteManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.VictorHugoBatista.personal_note_manager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalNoteManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
