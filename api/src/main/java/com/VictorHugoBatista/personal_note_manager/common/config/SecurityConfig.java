4bd694579144cf15b95d87c0cabd7794735b13d5
Victor Hugo Batista <vct.bta@gmail.com>
2025-10-23 22:18:07 -0300

    Add spring boot security and allow configs to the create user endpoint and swagger

---
+++ api/src/main/java/com/VictorHugoBatista/personal_note_manager/common/config/SecurityConfig.java
@@ -0,0 +1,23 @@
+package com.VictorHugoBatista.personal_note_manager.common.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf(csrf -> csrf.disable())
+            .authorizeHttpRequests(
+                authorize -> authorize
+                    .requestMatchers("/swagger-ui/**").permitAll()
+                    .requestMatchers("/v3/api-docs*/**").permitAll()
+                    .requestMatchers(HttpMethod.POST, "/api/v1/user").permitAll()
+                    .anyRequest().authenticated());
+
+        return http.build();
+    }
+}
